55a2478ace08978071d4b1f38de588e48df195e0
Peter Jones <pjones@devalot.com>
2022-06-22 13:33:33 -0700

    script: Add script for converting KDE config files to Nix

--- flake.nix
+++ flake.nix
@@ -12,12 +12,21 @@
     let
       # List of systems we run NixOS tests for:
       supportedSystems = [
-        "x86_64-linux"
+        "aarch64-darwin"
         "aarch64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
       ];
 
       # Function to generate a set based on supported systems:
       forAllSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
+
+      # Like `forAllSystems` except just those support NixOS tests:
+      forQemuSystems = inputs.nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
       # Attribute set of nixpkgs for each system:
       nixpkgsFor = forAllSystems (system:
@@ -28,7 +37,29 @@
         imports = [ ./modules ];
       };
 
-      checks = forAllSystems (system:
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system}; in
+        {
+          default = self.packages.${system}.rc2nix;
+
+          rc2nix = pkgs.writeShellApplication {
+            name = "rc2nix";
+            runtimeInputs = with pkgs; [ ruby ];
+            text = ''ruby ${script/rc2nix.rb} "$@"'';
+          };
+        });
+
+      apps = forAllSystems (system:
+        {
+          default = self.apps.${system}.rc2nix;
+
+          rc2nix = {
+            type = "app";
+            program = "${self.packages.${system}.rc2nix}/bin/rc2nix";
+          };
+        });
+
+      checks = forQemuSystems (system:
         let test = path: import path {
           pkgs = nixpkgsFor.${system};
           home-manager = inputs.home-manager;
---
+++ script/rc2nix.rb
@@ -0,0 +1,171 @@
+#!/usr/bin/env ruby
+
+################################################################################
+#
+# This file is part of the package Plasma Manager.  It is subject to
+# the license terms in the LICENSE file found in the top-level
+# directory of this distribution and at:
+#
+#   https://github.com/pjones/plasma-manager
+#
+# No part of this package, including this file, may be copied,
+# modified, propagated, or distributed except according to the terms
+# contained in the LICENSE file.
+#
+################################################################################
+require('optparse')
+
+################################################################################
+module Rc2Nix
+
+  ##############################################################################
+  # Files that we'll scan by default.
+  KNOWN_FILES = [
+    "kcminputrc",
+    "kglobalshortcutsrc",
+    "kactivitymanagerdrc",
+    "ksplashrc",
+    "kwin_rules_dialogrc",
+    "kmixrc",
+    "kwalletrc",
+    "kgammarc",
+    "ktimezonedrc",
+    "krunnerrc",
+    "klaunchrc",
+    "plasmanotifyrc",
+    "systemsettingsrc",
+    "kscreenlockerrc",
+    "kwinrulesrc",
+    "plasmashellrc",
+    "khotkeysrc",
+    "ksmserverrc",
+    "kded5rc",
+    "plasmarc",
+    "kwinrc",
+    "startkderc",
+    "kdeglobals",
+    "baloofilerc",
+    "dolphinrc",
+    "klipperrc",
+    "plasma-localerc",
+    "kxkbrc",
+  ].map {|f| File.expand_path(f, ENV["XDG_CONFIG_HOME"] || "~/.config")}.freeze
+
+  ##############################################################################
+  class RcFile
+
+    ############################################################################
+    attr_reader(:file_name, :settings)
+
+    ############################################################################
+    def initialize(file_name)
+      @file_name = file_name
+      @settings = {}
+      @last_group = nil
+    end
+
+    ############################################################################
+    def parse
+      File.open(@file_name) do |file|
+        file.each do |line|
+          case line
+          when /^\s*$/
+            next
+          when /^\s*(\[[^\]]+\]){1,}\s*$/
+            @last_group = parse_group(line.strip)
+          when /^\s*([^=]+)=?(.*)\s*$/
+            if @last_group.nil?
+              raise("#{@file_name}: setting outside of group: #{line}")
+            end
+
+            key = $1.strip
+            val = $2.strip
+
+            @settings[@last_group] ||= {}
+            @settings[@last_group][key] = val
+          else
+            raise("#{@file_name}: can't parse line: #{line}")
+          end
+        end
+      end
+    end
+
+    ############################################################################
+    def parse_group(line)
+      line.gsub(/\s*\[([^\]]+)\]\s*/) do |match|
+        $1 + "."
+      end.sub(/\.$/, '')
+    end
+  end
+
+  ##############################################################################
+  class App
+
+    ############################################################################
+    def initialize(args)
+      @files = KNOWN_FILES.dup
+
+      OptionParser.new do |p|
+        p.on("-h", "--help", "This message") {$stdout.puts(p); exit}
+
+        p.on("-c", "--clear", "Clear the file list") do
+          @files = []
+        end
+
+        p.on("-a", "--add=FILE", "Add a file to the scan list") do |file|
+          @files << File.expand_path(file)
+        end
+      end.parse!(args)
+    end
+
+    ############################################################################
+    def run
+      settings = {}
+
+      @files.each do |file|
+        rc = RcFile.new(file)
+        rc.parse
+        settings[File.basename(file)] = rc.settings
+      end
+
+      puts("{")
+      puts("  programs.plasma = {")
+      puts("    enable = true;")
+      puts("    files = {")
+      pp_settings(settings, 6)
+      puts("    };")
+      puts("  };")
+      puts("}")
+    end
+
+    ############################################################################
+    def pp_settings(settings, indent)
+      settings.keys.sort.each do |file|
+        settings[file].keys.sort.each do |group|
+          settings[file][group].keys.sort.each do |key|
+            print(" " * indent)
+            print("\"#{file}\".")
+            print("\"#{group}\".")
+            print("\"#{key}\" = ")
+            print(nix_val(settings[file][group][key]))
+            print(";\n")
+          end
+        end
+      end
+    end
+
+    ############################################################################
+    def nix_val(str)
+      case str
+      when /^true|false$/i
+        str.downcase
+      when /^[0-9]+(\.[0-9]+)?$/
+        str
+      else
+        '"' + str.gsub(/(?<!\\)"/, '\\"') + '"'
+      end
+    end
+  end
+end
+
+Rc2Nix::App.new(ARGV).run
