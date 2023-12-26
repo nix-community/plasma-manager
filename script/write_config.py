197a956e2f1417f2819b6abe1c31bd751999c29f
magnouvean <c6af1d08-d039-472f-b890-6eaf2e96e961@anonaddy.me>
2023-12-26 15:23:05 +0100

    Replace configparser with custom-written solution

--- script/write_config.py
+++ script/write_config.py
@@ -1,18 +1,89 @@
 import configparser
 import json
+import re
 import sys
 from typing import Dict
 
 
-def write_config_single(filename: str, items: Dict):
-    config = configparser.ConfigParser(interpolation=None)
-    config.optionxform = str
-    config.read(filename)
+class KConfParser:
+    def __init__(self, filepath):
+        self.data = {}
+        self.filepath = filepath
+        self.read()
+
+    def read(self):
+        """Read the config from the path specified on instantiation. If the
+        path doesn't exist, do nothing"""
+        try:
+            with open(self.filepath, "r") as f:
+                current_group = 0
+                for l in f:
+                    # Checks if the current line indicates a group.
+                    if re.match(r"^\[.*\]\s*$", l):
+                        current_group = l.rstrip().lstrip("[").rstrip("]")
+                        self.data[current_group] = {}
+                        continue
+
+                    # Empty lines we won't bother reading.
+                    if l.strip() != "":
+                        key, value = self.get_key_value(l)
+                        self.set_value(current_group, key, value)
+        except FileNotFoundError:
+            pass
+
+    def set_value(self, group, key, value):
+        """Adds an entry to the config. Creates necessary groups if needed."""
+        if not group in self.data:
+            self.data[group] = {}
+        self.data[group][key] = value
+
+    def remove_value(self, group, key):
+        """Removes an entry from the config. Does nothing if the entry isn't there."""
+        if group in self.data and key in self.data[group]:
+            del self.data[group][key]
+            # We remove the group altogether if there are no keys in the group
+            # anymore.
+            if not self.data[group]:
+                del self.data[group]
+
+    def save(self):
+        """Save to the filepath specified on instantiation."""
+        with open(self.filepath, "w") as f:
+            # We skip a newline before the first category
+            skip_newline = True
+
+            for group in self.data:
+                if group == 0:
+                    skip_newline = False
+                    for key, value in self.data[0].items():
+                        f.write(f"{self.key_value_to_line(key, value)}\n")
+                    continue
+
+                if not skip_newline:
+                    f.write("\n")
+                else:
+                    # Only skip the newline once of course.
+                    skip_newline = False
+
+                f.write(f"[{group}]\n")
+                for key, value in self.data[group].items():
+                    f.write(f"{self.key_value_to_line(key, value)}\n")
+
+    @staticmethod
+    def get_key_value(line: str) -> tuple[str, str]:
+        key, value = line.split("=", 1)
+        return key.strip(), value.strip()
+
+    @staticmethod
+    def key_value_to_line(key: str, value: str) -> str:
+        return f"{key}={value}"
+
+
+def write_config_single(filepath: str, items: Dict):
+    config = KConfParser(filepath)
 
     for entry in items.values():
         group = f"{']['.join(entry['configGroupNesting'])}"
-        if not group in config:
-            config.add_section(group)
 
         for key, value in entry.items():
             if key == "configGroupNesting":
@@ -22,24 +93,20 @@ def write_config_single(filename: str, items: Dict):
             # we remove the key/option (and the group/section if it is empty
             # after removal).
             if value is None:
-                if key in config[group]:
-                    config.remove_option(group, key)
-                if not config[group]:
-                    config.remove_section(group)
+                config.remove_value(group, key)
                 continue
 
             # Convert value to string.
             value = str(value) if not isinstance(value, bool) else str(value).lower()
 
-            config[group][key] = value
+            config.set_value(group, key, value)
 
-    with open(filename, "w") as f:
-        config.write(f, space_around_delimiters=False)
+    config.save()
 
 
 def write_configs(d: Dict):
-    for filename, c in d.items():
-        write_config_single(filename, c)
+    for filepath, c in d.items():
+        write_config_single(filepath, c)
 
 
 def main():
