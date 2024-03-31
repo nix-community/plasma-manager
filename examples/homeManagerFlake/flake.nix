88cf932923835c05f26176a8a4df30a0dd1e7775
magnouvean <85435692+magnouvean@users.noreply.github.com>
2024-03-31 23:29:18 +0200

    Major updates to readme and example flakes (#104)
    Co-authored-by: blitter <blitter@blaupost.de>

---
+++ examples/homeManagerFlake/flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Plasma Manager Example with standalone home-manager flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    plasma-manager = {
+      url = "github:pjones/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+  };
+
+  outputs = inputs@ { nixpkgs, home-manager, plasma-manager, ... }:
+    let
+      # Replace with your username
+      username = "jdoe";
+
+      # Replace with the fitting architecture
+      system = "x86_64-linux";
+    in
+    {
+      # Replace `standAloneConfig` with the name of your configuration (your `username` or `"username@hostname"`)
+      homeConfigurations.standAloneConfig = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs { inherit system; };
+
+        modules = [
+          inputs.plasma-manager.homeManagerModules.plasma-manager
+
+          # Specify the path to your home configuration here:
+          ../home.nix
+
+          {
+            home = {
+              inherit username;
+              homeDirectory = "/home/${username}";
+            };
+          }
+        ];
+      };
+    };
+}
---
+++ examples/systemFlake/flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Plasma Manager Example with system configuration flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    plasma-manager = {
+      url = "github:pjones/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+  };
+
+  outputs = inputs@ { nixpkgs, home-manager, plasma-manager, ... }:
+    let
+      # Replace with your username
+      username = "jdoe";
+
+      # Replace with the fitting architecture
+      system = "x86_64-linux";
+    in
+    {
+      # Replace `moduleConfig` with the name of you configuration
+      nixosConfigurations.moduleConfig = nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        modules = [
+          # We include the system-configuration here as well. Replace this with
+          # your own configuration or import your configuration.nix. The demo
+          # here is just the bare minimum to get the flake to not fail.
+          {
+            system.stateVersion = "23.11";
+            users.users."${username}".isNormalUser = true;
+            fileSystems."/".device = "/dev/sda";
+            boot.loader.grub.devices = [ "/dev/sda" ];
+          }
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
+
+            # This should point to your home.nix path of course. For an example
+            # of this see ./home.nix in this directory.
+            home-manager.users."${username}" = import ../home.nix;
+          }
+        ];
+      };
+    };
+}
