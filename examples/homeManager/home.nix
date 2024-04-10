61330e4017e23abd05f2fd78bf0aed0ee4f62ab3
nat-418 <93013864+nat-418@users.noreply.github.com>
2024-04-10 21:03:26 +0200

    docs: add standard Nix / Home Manager example

---
+++ examples/homeManager/home.nix
@@ -0,0 +1,82 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    <plasma-manager/modules>
+  ];
+
+  programs.plasma = {
+    enable = true;
+
+    #
+    # Some high-level settings:
+    #
+    workspace = {
+      clickItemTo = "select";
+      lookAndFeel = "org.kde.breezedark.desktop";
+      cursorTheme = "Bibata-Modern-Ice";
+      iconTheme = "Papirus-Dark";
+      wallpaper = "${pkgs.libsForQt5.plasma-workspace-wallpapers}/share/wallpapers/Patak/contents/images/1080x1920.png";
+    };
+
+    hotkeys.commands."launch-konsole" = {
+      name = "Launch Konsole";
+      key = "Meta+Alt+K";
+      command = "konsole";
+    };
+
+    panels = [
+      # Windows-like panel at the bottom
+      {
+        location = "bottom";
+        widgets = [
+          "org.kde.plasma.kickoff"
+          "org.kde.plasma.icontasks"
+          "org.kde.plasma.marginsseperator"
+          "org.kde.plasma.systemtray"
+          "org.kde.plasma.digitalclock"
+        ];
+      }
+      # Global menu at the top
+      {
+        location = "top";
+        height = 26;
+        widgets = [
+          "org.kde.plasma.appmenu"
+        ];
+      }
+    ];
+
+
+    #
+    # Some mid-level settings:
+    #
+    shortcuts = {
+      ksmserver = {
+        "Lock Session" = [ "Screensaver" "Meta+Ctrl+Alt+L" ];
+      };
+
+      kwin = {
+        "Expose" = "Meta+,";
+        "Switch Window Down" = "Meta+J";
+        "Switch Window Left" = "Meta+H";
+        "Switch Window Right" = "Meta+L";
+        "Switch Window Up" = "Meta+K";
+      };
+    };
+
+
+    #
+    # Some low-level settings:
+    #
+    configFile = {
+      "baloofilerc"."Basic Settings"."Indexing-Enabled".value = false;
+      "kwinrc"."org.kde.kdecoration2"."ButtonsOnLeft".value = "SF";
+      "kwinrc"."Desktops"."Number" = {
+        value = 8;
+        # Forces kde to not change this value (even through the settings app).
+        immutable = true;
+      };
+    };
+  };
+}
