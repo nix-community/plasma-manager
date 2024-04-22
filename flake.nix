4a1e1d534a08533f20f689b0746ba72f30026879
magnouvean <c6af1d08-d039-472f-b890-6eaf2e96e961@anonaddy.me>
2024-04-22 16:51:11 +0200

    Update remove vm to plasma 6

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Manage KDE Plasma with Home Manager";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
@@ -41,7 +41,7 @@
                 home-manager-module = inputs.home-manager.nixosModules.home-manager;
                 plasma-module = self.homeManagerModules.plasma-manager;
               })
-              (_: {environment.systemPackages = [ self.packages.${system}.rc2nix]; })
+              (_: { environment.systemPackages = [ self.packages.${system}.rc2nix ]; })
             ];
           }).config.system.build.vm;
 
