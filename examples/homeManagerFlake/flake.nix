637622c672d68cf4d525c75ffde425a7a9a54572
Toast <39011842+toast003@users.noreply.github.com>
2024-06-24 12:40:17 +0200

    Replace mentions of old repo location with the new one

--- examples/homeManagerFlake/flake.nix
+++ examples/homeManagerFlake/flake.nix
@@ -8,7 +8,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     plasma-manager = {
-      url = "github:pjones/plasma-manager";
+      url = "github:nix-community/plasma-manager";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
