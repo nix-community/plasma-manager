eba2f7fa4ca4f1ab3cc93885a787fa18a3d2d9d2
Toast <39011842+toast003@users.noreply.github.com>
2024-07-01 10:58:42 +0200

    Add basic trackpad settings

--- modules/input.nix
+++ modules/input.nix
@@ -39,14 +39,69 @@ in
       )
     ]
   );
-  config.programs.plasma.configFile."kcminputrc" = mkIf (cfg.enable) (
-    mkMerge [
-      (
-        # For some reason the numlock settings are here and not on kxkbrc?
-        mkIf (cfg.input.keyboard.numlockOnStartup != null) {
-          Keyboard.NumLock.value = lists.findFirstIndex (x: x == cfg.input.keyboard.numlockOnStartup) null numlockSettings;
-        }
-      )
-    ]
-  );
+
+  # Trackpad options
+
+  options.programs.plasma.input.touchpad = {
+    enable = mkOption {
+      type = with types; nullOr bool;
+      default = null;
+      example = true;
+      description = ''
+        Enables or disables the trackpad
+      '';
+    };
+    name = mkOption {
+      type = with types; nullOr str;
+      default = null;
+      example = "PNP0C50:00 0911:5288 Touchpad";
+      description = ''
+        The name of the trackpad.
+        You can find it out running cat /proc/bus/input/devices | grep -i touchpad
+      '';
+    };
+    # Plasma writes down the IDs as decimal, I could do the conversion here but I looked how to do it and it's rough
+    # We should do this on write_config.py
+    # https://github.com/Misterio77/nix-colors/blob/b92df8f5eb1fa20d8e09810c03c9dc0d94ef2820/lib/core/conversions.nix#L87
+    vendorId = mkOption {
+      type = with types; nullOr str;
+      default = null;
+      example = "2321";
+      description = ''
+        The vendor ID of the trackpad, in decimal.
+        You can find it out running cat /proc/bus/input/devices | grep -i touchpad
+      '';
+    };
+    productId = mkOption {
+      type = with types; nullOr str;
+      default = null;
+      example = "21128";
+      description = ''
+        The product ID of the trackpad, in decimal.
+        You can find it out running cat /proc/bus/input/devices | grep -i touchpad
+      '';
+    };
+  };
+
+  config.programs.plasma.configFile."kcminputrc" =
+    let
+      touchName = cfg.input.touchpad.name;
+      touchVendor = cfg.input.touchpad.vendorId;
+      touchProduct = cfg.input.touchpad.productId;
+    in
+    mkIf (cfg.enable) (
+      mkMerge [
+        (
+          # For some reason the numlock settings are here and not on kxkbrc?
+          mkIf (cfg.input.keyboard.numlockOnStartup != null) {
+            Keyboard.NumLock.value = lists.findFirstIndex (x: x == cfg.input.keyboard.numlockOnStartup) null numlockSettings;
+          }
+        )
+        (
+          mkIf (cfg.input.touchpad.enable != null) {
+            "Libinput/${touchVendor}/${touchProduct}/${touchName}".Enabled.value = cfg.input.touchpad.enable;
+          }
+        )
+      ]
+    );
 }
