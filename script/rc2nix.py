547fc24e1fdc9bf736930123e49d62a96f84836e
LuoChen <luochen1990@gmail.com>
2024-07-10 21:13:24 +0800

    add type annotation for rc2nix.py

--- script/rc2nix.py
+++ script/rc2nix.py
@@ -18,25 +18,48 @@ import os
 import re
 import sys
 from pathlib import Path
+from typing import List, Dict, Callable, Optional, Tuple
 
 # The root directory where configuration files are stored.
-XDG_CONFIG_HOME = os.path.expanduser(os.getenv("XDG_CONFIG_HOME", "~/.config"))
+XDG_CONFIG_HOME: str = os.path.expanduser(os.getenv("XDG_CONFIG_HOME", "~/.config"))
 
 class Rc2Nix:
     # Files that we'll scan by default.
-    KNOWN_FILES = [
-        "kcminputrc", "kglobalshortcutsrc", "kactivitymanagerdrc", "ksplashrc",
-        "kwin_rules_dialogrc", "kmixrc", "kwalletrc", "kgammarc", "krunnerrc",
-        "klaunchrc", "plasmanotifyrc", "systemsettingsrc", "kscreenlockerrc",
-        "kwinrulesrc", "khotkeysrc", "ksmserverrc", "kded5rc", "plasmarc",
-        "kwinrc", "kdeglobals", "baloofilerc", "dolphinrc", "klipperrc",
-        "plasma-localerc", "kxkbrc", "ffmpegthumbsrc", "kservicemenurc", "kiorc"
+    KNOWN_FILES: List[str] = [
+        "kcminputrc",
+        "kglobalshortcutsrc",
+        "kactivitymanagerdrc",
+        "ksplashrc",
+        "kwin_rules_dialogrc",
+        "kmixrc",
+        "kwalletrc",
+        "kgammarc",
+        "krunnerrc",
+        "klaunchrc",
+        "plasmanotifyrc",
+        "systemsettingsrc",
+        "kscreenlockerrc",
+        "kwinrulesrc",
+        "khotkeysrc",
+        "ksmserverrc",
+        "kded5rc",
+        "plasmarc",
+        "kwinrc",
+        "kdeglobals",
+        "baloofilerc",
+        "dolphinrc",
+        "klipperrc",
+        "plasma-localerc",
+        "kxkbrc",
+        "ffmpegthumbsrc",
+        "kservicemenurc",
+        "kiorc",
     ]
-    KNOWN_FILES = [os.path.join(XDG_CONFIG_HOME, f) for f in KNOWN_FILES]
+    KNOWN_FILES: List[str] = [os.path.join(XDG_CONFIG_HOME, f) for f in KNOWN_FILES]
 
     class RcFile:
         # Any group that matches a listed regular expression is blocked
-        GROUP_BLOCK_LIST = [
+        GROUP_BLOCK_LIST: List[str] = [
             r"^(ConfigDialog|FileDialogSize|ViewPropertiesDialog|KPropertiesDialog)$",
             r"^\$Version$",
             r"^ColorEffects:",
@@ -50,7 +73,7 @@ class Rc2Nix:
         ]
 
         # Similar to the GROUP_BLOCK_LIST but for setting keys.
-        KEY_BLOCK_LIST = [
+        KEY_BLOCK_LIST: List[str] = [
             r"^activate widget \d+$",  # Depends on state :(
             r"^ColorScheme(Hash)?$",
             r"^History Items",
@@ -63,14 +86,14 @@ class Rc2Nix:
         ]
 
         # List of functions that get called with a group name and a key name.
-        BLOCK_LIST_LAMBDA = [
+        BLOCK_LIST_LAMBDA: List[Callable[[str, str], bool]] = [
             lambda group, key: group == "org.kde.kdecoration2" and key == "library"
         ]
 
-        def __init__(self, file_name):
-            self.file_name = file_name
-            self.settings = {}
-            self.last_group = None
+        def __init__(self, file_name: str):
+            self.file_name: str = file_name
+            self.settings: Dict[str, Dict[str, str]] = {}
+            self.last_group: Optional[str] = None
 
         def parse(self):
             with open(self.file_name, 'r') as file:
@@ -86,19 +109,22 @@ class Rc2Nix:
                     else:
                         raise Exception(f"{self.file_name}: can't parse line: {line}")
 
-        def is_group_line(self, line):
-            return re.match(r'^\s*(\[[^\]]+\]){1,}\s*$', line)
+        def is_group_line(self, line: str) -> bool:
+            return re.match(r'^\s*(\[[^\]]+\]){1,}\s*$', line) is not None
 
-        def is_setting_line(self, line):
-            return re.match(r'^\s*([^=]+)=?(.*)\s*$', line)
+        def is_setting_line(self, line: str) -> bool:
+            return re.match(r'^\s*([^=]+)=?(.*)\s*$', line) is not None
 
-        def parse_group(self, line):
+        def parse_group(self, line: str) -> str:
             return re.sub(r'\s*\[([^\]]+)\]\s*', r'\1/', line.replace("/", "\\/")).rstrip("/")
 
-        def parse_setting(self, line):
-            return re.match(r'^\s*([^=]+)=?(.*)\s*$', line).groups()
+        def parse_setting(self, line: str) -> Tuple[str, str]:
+            match = re.match(r'^\s*([^=]+)=?(.*)\s*$', line)
+            if match:
+                return match.groups()
+            raise Exception(f"{self.file_name}: can't parse setting line: {line}")
 
-        def process_setting(self, key, val):
+        def process_setting(self, key: str, val: str):
             key = key.strip()
             val = val.strip()
 
@@ -112,21 +138,21 @@ class Rc2Nix:
                 self.settings[self.last_group] = {}
             self.settings[self.last_group][key] = val
 
-        def should_skip_group(self, group):
+        def should_skip_group(self, group: str) -> bool:
             return any(re.match(reg, group) for reg in self.GROUP_BLOCK_LIST)
 
-        def should_skip_key(self, key):
+        def should_skip_key(self, key: str) -> bool:
             return any(re.match(reg, key) for reg in self.KEY_BLOCK_LIST)
 
-        def should_skip_by_lambda(self, group, key):
+        def should_skip_by_lambda(self, group: str, key: str) -> bool:
             return any(fn(group, key) for fn in self.BLOCK_LIST_LAMBDA)
 
     class App:
-        def __init__(self, args):
-            self.files = Rc2Nix.KNOWN_FILES.copy()
+        def __init__(self, args: List[str]):
+            self.files: List[str] = Rc2Nix.KNOWN_FILES.copy()
 
         def run(self):
-            settings = {}
+            settings: Dict[str, Dict[str, Dict[str, str]]] = {}
 
             for file in self.files:
                 if not os.path.exists(file):
@@ -145,63 +171,56 @@ class Rc2Nix:
             print("  programs.plasma = {")
             print("    enable = true;")
             print("    shortcuts = {")
-            self.pp_shortcuts(settings.get("kglobalshortcutsrc", {}), 6)
+            print(self.pp_shortcuts(settings.get("kglobalshortcutsrc", {}), 6))
             print("    };")
             print("    configFile = {")
-            self.pp_settings(settings, 6)
+            print(self.pp_settings(settings, 6))
             print("    };")
             print("  };")
             print("}")
 
-        def pp_settings(self, settings, indent):
+        def pp_settings(self, settings: Dict[str, Dict[str, Dict[str, str]]], indent: int) -> str:
+            result = []
             for file in sorted(settings.keys()):
                 for group in sorted(settings[file].keys()):
                     for key in sorted(settings[file][group].keys()):
                         if file == "kglobalshortcutsrc" and key != "_k_friendly_name":
                             continue
 
-                        print(" " * indent, end="")
-                        print(f"\"{file}\".", end="")
-                        print(f"\"{group}\".", end="")
-                        print(f"\"{key}\" = ", end="")
-                        print(self.nix_val(settings[file][group][key]), end="")
-                        print(";")
+                        result.append(f"{' ' * indent}\"{file}\".\"{group}\".\"{key}\" = {nix_val(settings[file][group][key])};")
+            return "\n".join(result)
 
-        def pp_shortcuts(self, groups, indent):
+        def pp_shortcuts(self, groups: Dict[str, Dict[str, str]], indent: int) -> str:
             if not groups:
-                return
+                return ""
 
+            result = []
             for group in sorted(groups.keys()):
                 for action in sorted(groups[group].keys()):
                     if action == "_k_friendly_name":
                         continue
 
-                    print(" " * indent, end="")
-                    print(f"\"{group}\".", end="")
-                    print(f"\"{action}\" = ", end="")
-
                     keys = groups[group][action].split(r'(?<!\\),')[0].replace(r'\?', ',').replace(r'\t', '\t').split('\t')
 
                     if not keys:
-                        print("[ ]", end="")
+                        keys_str = "[ ]"
                     elif len(keys) > 1:
-                        print("[", end="")
-                        print(" ".join(self.nix_val(k) for k in keys), end="")
-                        print("]", end="")
+                        keys_str = f"[ {' '.join(nix_val(k) for k in keys)} ]"
                     elif keys[0] == "none":
-                        print("[ ]", end="")
+                        keys_str = "[ ]"
                     else:
-                        print(self.nix_val(keys[0]), end="")
+                        keys_str = nix_val(keys[0])
 
-                    print(";")
+                    result.append(f"{' ' * indent}\"{group}\".\"{action}\" = {keys_str};")
+            return "\n".join(result)
 
-        def nix_val(self, str):
-            if str is None:
-                return "null"
-            if re.match(r'^true|false$', str, re.IGNORECASE):
-                return str.lower()
-            if re.match(r'^[0-9]+(\.[0-9]+)?$', str):
-                return str
-            return '"' + str.replace('"', '\\"') + '"'
+def nix_val(s: str) -> str:
+    if s is None:
+        return "null"
+    if re.match(r'^true|false$', s, re.IGNORECASE):
+        return s.lower()
+    if re.match(r'^[0-9]+(\.[0-9]+)?$', s):
+        return s
+    return '"' + s.replace('"', '\\"') + '"'
 
 Rc2Nix.App(sys.argv[1:]).run()
