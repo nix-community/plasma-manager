57a0600f576994e7648b656eadfe112d7690ab98
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-07-28 20:46:14 +0000

    Fix middleButtonEmulation example in input module

--- modules/input.nix
+++ modules/input.nix
@@ -371,7 +371,7 @@ in
         vendorId = "046d";
         productId = "c08f";
         leftHanded = false;
-        middleMouseEmulation = false;
+        middleButtonEmulation = false;
         acceleration = 0.5;
         accelerationProfile = "none";
         naturalScroll = false;
