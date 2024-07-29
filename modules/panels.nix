cd56a69eb56d9294d99d1135da5ff37496d63f8f
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-07-29 16:23:46 +0000

    Add applet-window-button6 to additionalWidgetPackages

--- modules/panels.nix
+++ modules/panels.nix
@@ -12,6 +12,7 @@ let
   additionalWidgetPackages = with pkgs; {
     "com.github.antroids.application-title-bar" = [ application-title-bar ];
     plasmusic-toolbar = [ plasmusic-toolbar ];
+    "org.kde.windowbuttons" = [ kdePackages.applet-window-buttons6 ];
   };
   # An attrset of service-names and widgets/conditions. If any of the
   # conditions (given in cond) evaluate to true for any of the widgets with the
