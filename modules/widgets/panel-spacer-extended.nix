e08c14397fc1489bfa1d4e1478d50bbfbadfbb9a
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-07-31 14:57:35 +0000

    Add screenWidth option and filter null attribute values in actions

--- modules/widgets/panel-spacer-extended.nix
+++ modules/widgets/panel-spacer-extended.nix
@@ -77,6 +77,12 @@ in {
         example = true;
         description = "Whether to show list of actions when hovering the spacer.";
       };
+      screenWidth = mkOption {
+        type = types.nullOr types.ints.positive;
+        default = null;
+        example = 1920;
+        description = "The screen width in pixels.";
+      };
       scrollThreshold = mkOption {
         type = types.nullOr types.ints.unsigned;
         default = null;
@@ -99,11 +105,11 @@ in {
           description = "The action to perform on single click.";
           apply = singleClick: let
             convertAction = action: component: "${component},${action}";
-          in lib.optionalAttrs (singleClick != null) {
+          in lib.optionalAttrs (singleClick != null) lib.filterAttrs (_: v: v != null) ({
             singleClickAction = convertAction singleClick.action singleClick.component;
             singleClickCommand = singleClick.command;
             singleClickAppUrl = singleClick.appUrl;
-          };
+          });
         };
         doubleClick = mkOption {
           type = types.nullOr actionType;
@@ -116,11 +122,11 @@ in {
           description = "The action to perform on double click.";
           apply = doubleClick: let
             convertAction = action: component: "${component},${action}";
-          in lib.optionalAttrs (doubleClick != null) {
+          in lib.optionalAttrs (doubleClick != null) lib.filterAttrs (_: v: v != null) ({
             doubleClickAction = convertAction doubleClick.action doubleClick.component;
             doubleClickCommand = doubleClick.command;
             doubleClickAppUrl = doubleClick.appUrl;
-          };
+          });
         };
         middleClick = mkOption {
           type = types.nullOr actionType;
@@ -133,11 +139,11 @@ in {
           description = "The action to perform on middle click.";
           apply = middleClick: let
             convertAction = action: component: "${component},${action}";
-          in lib.optionalAttrs (middleClick != null) {
+          in lib.optionalAttrs (middleClick != null) lib.filterAttrs (_: v: v != null) ({
             middleClickAction = convertAction middleClick.action middleClick.component;
             middleClickCommand = middleClick.command;
             middleClickAppUrl = middleClick.appUrl;
-          };
+          });
         };
         mouseWheelUp = mkOption {
           type = types.nullOr actionType;
@@ -150,11 +156,11 @@ in {
           description = "The action to perform on mouse wheel up.";
           apply = mouseWheelUp: let
             convertAction = action: component: "${component},${action}";
-          in lib.optionalAttrs (mouseWheelUp != null) {
+          in lib.optionalAttrs (mouseWheelUp != null) lib.filterAttrs (_: v: v != null) ({
             mouseWheelUpAction = convertAction mouseWheelUp.action mouseWheelUp.component;
             mouseWheelUpCommand = mouseWheelUp.command;
             mouseWheelUpAppUrl = mouseWheelUp.appUrl;
-          };
+          });
         };
         mouseWheelDown = mkOption {
           type = types.nullOr actionType;
@@ -167,11 +173,11 @@ in {
           description = "The action to perform on mouse wheel down.";
           apply = mouseWheelDown: let
             convertAction = action: component: "${component},${action}";
-          in lib.optionalAttrs (mouseWheelDown != null) {
+          in lib.optionalAttrs (mouseWheelDown != null) lib.filterAttrs (_: v: v != null) ({
             mouseWheelDownAction = convertAction mouseWheelDown.action mouseWheelDown.component;
             mouseWheelDownCommand = mouseWheelDown.command;
             mouseWheelDownAppUrl = mouseWheelDown.appUrl;
-          };
+          });
         };
         mouseDragUp = mkOption {
           type = types.nullOr actionType;
@@ -184,11 +190,11 @@ in {
           description = "The action to perform on mouse drag up.";
           apply = mouseDragUp: let
             convertAction = action: component: "${component},${action}";
-          in lib.optionalAttrs (mouseDragUp != null) {
+          in lib.optionalAttrs (mouseDragUp != null) lib.filterAttrs (_: v: v != null) ({
             mouseDragUpAction = convertAction mouseDragUp.action mouseDragUp.component;
             mouseDragUpCommand = mouseDragUp.command;
             mouseDragUpAppUrl = mouseDragUp.appUrl;
-          };
+          });
         };
         mouseDragDown = mkOption {
           type = types.nullOr actionType;
@@ -201,11 +207,11 @@ in {
           description = "The action to perform on mouse drag down.";
           apply = mouseDragDown: let
             convertAction = action: component: "${component},${action}";
-          in lib.optionalAttrs (mouseDragDown != null) {
+          in lib.optionalAttrs (mouseDragDown != null) lib.filterAttrs (_: v: v != null) ({
             mouseDragDownAction = convertAction mouseDragDown.action mouseDragDown.component;
             mouseDragDownCommand = mouseDragDown.command;
             mouseDragDownAppUrl = mouseDragDown.appUrl;
-          };
+          });
         };
         mouseDragLeft = mkOption {
           type = types.nullOr actionType;
@@ -218,11 +224,11 @@ in {
           description = "The action to perform on mouse drag left.";
           apply = mouseDragLeft: let
             convertAction = action: component: "${component},${action}";
-          in lib.optionalAttrs (mouseDragLeft != null) {
+          in lib.optionalAttrs (mouseDragLeft != null) lib.filterAttrs (_: v: v != null) ({
             mouseDragLeftAction = convertAction mouseDragLeft.action mouseDragLeft.component;
             mouseDragLeftCommand = mouseDragLeft.command;
             mouseDragLeftAppUrl = mouseDragLeft.appUrl;
-          };
+          });
         };
         mouseDragRight = mkOption {
           type = types.nullOr actionType;
@@ -235,11 +241,11 @@ in {
           description = "The action to perform on mouse drag right.";
           apply = mouseDragRight: let
             convertAction = action: component: "${component},${action}";
-          in lib.optionalAttrs (mouseDragRight != null) {
+          in lib.optionalAttrs (mouseDragRight != null) lib.filterAttrs (_: v: v != null) ({
             mouseDragRightAction = convertAction mouseDragRight.action mouseDragRight.component;
             mouseDragRightCommand = mouseDragRight.command;
             mouseDragRightAppUrl = mouseDragRight.appUrl;
-          };
+          });
         };
         longPress = mkOption {
           type = types.nullOr actionType;
@@ -252,11 +258,11 @@ in {
           description = "The action to perform on long press.";
           apply = longPress: let
             convertAction = action: component: "${component},${action}";
-          in lib.optionalAttrs (longPress != null) {
+          in lib.optionalAttrs (longPress != null) lib.filterAttrs (_: v: v != null) ({
             pressHoldAction = convertAction longPress.action longPress.component;
             pressHoldCommand = longPress.command;
             pressHoldAppUrl = longPress.appUrl;
-          };
+          });
         };
       };
       troubleshooting = {
@@ -289,6 +295,7 @@ in {
       , length
       , highlight
       , showTooltip
+      , screenWidth
       , scrollThreshold
       , actions
       , troubleshooting
@@ -298,7 +305,7 @@ in {
       config = lib.recursiveUpdate {
         General = lib.filterAttrs (_: v: v != null) (
           {
-            inherit expanding length showTooltip;
+            inherit expanding length showTooltip screenWidth;
             
             showHoverBg = highlight.enable;
             hoverBgRadius = highlight.radius;
