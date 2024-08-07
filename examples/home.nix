0afcd73353d58db151b858b2fb89d1e8a26269e4
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-08-07 15:52:08 +0000

    Add polonium example

--- examples/home.nix
+++ examples/home.nix
@@ -236,6 +236,8 @@
     kwin = {
       edgeBarrier = 0; # Disables the edge-barriers introduced in plasma 6.1
       cornerBarrier = false;
+
+      scripts.polonium.enable = true;
     };
 
     kscreenlocker = {
