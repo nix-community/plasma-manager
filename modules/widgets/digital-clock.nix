82d56572a6600a89e195d0d63d6534b60caf1c51
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-08-13 14:14:56 -0300

    Update settings option value type for digital clock

--- modules/widgets/digital-clock.nix
+++ modules/widgets/digital-clock.nix
@@ -1,6 +1,7 @@
 { lib, ... }:
 let
   inherit (lib) mkOption types;
+  inherit (import ./lib.nix { inherit lib; }) configValueType;
 
   mkBoolOption = description: mkOption {
     type = with types; nullOr bool;
@@ -220,7 +221,7 @@ in
           };
       };
       settings = mkOption {
-        type = with types; nullOr (attrsOf (attrsOf (either (oneOf [ bool float int str ]) (listOf (oneOf [ bool float int str ])))));
+        type = configValueType;
         default = null;
         example = {
           Appearance = {
