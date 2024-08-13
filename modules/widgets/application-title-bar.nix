53c7dbe1fcb3a68b7b0eb8c44fa6c9694ab445ce
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-08-13 14:13:17 -0300

    Update settings option value type for application-title-bar

--- modules/widgets/application-title-bar.nix
+++ modules/widgets/application-title-bar.nix
@@ -1,6 +1,7 @@
 { lib, ... }:
 let
   inherit (lib) mkOption types;
+  inherit (import ./lib.nix { inherit lib; }) configValueType;
 
   mkBoolOption = description: lib.mkOption {
     type = with lib.types; nullOr bool;
@@ -438,7 +439,7 @@ in
         };
       };
       settings = mkOption {
-        type = with types; nullOr (attrsOf (attrsOf (either (oneOf [ bool float int str ]) (listOf (oneOf [ bool float int str ])))));
+        type = configValueType;
         default = null;
         example = {
           Appearance = {
