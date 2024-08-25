7ccd49fd867126fe61c5ab8405524f4b7e60cca1
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-08-25 01:19:30 +0000

    Add appmenu widget-specific options

---
+++ modules/widgets/app-menu.nix
@@ -0,0 +1,55 @@
+{ lib, ... }:
+let
+  inherit (import ./lib.nix { inherit lib; }) configValueType;
+  inherit (import ./default.nix { inherit lib; }) positionType sizeType;
+
+  mkBoolOption = description: lib.mkOption {
+    type = with lib.types; nullOr bool;
+    default = null;
+    example = true;
+    inherit description;
+  };
+in {
+  appMenu = {
+    opts = {
+      position = lib.mkOption {
+        type = positionType;
+        example = { horizontal = 100; vertical = 300; };
+        description = "The position of the widget. (Only for desktop widget)";
+      };
+      size = lib.mkOption {
+        type = sizeType;
+        example = { width = 500; height = 50; };
+        description = "The size of the widget. (Only for desktop widget)";
+      };
+      compactView = mkBoolOption "Whether to show the app menu in a compact view";
+      settings = lib.mkOption {
+        type = configValueType;
+        default = null;
+        example = {
+          Appearance = {
+            compactView = true;
+          };
+        };
+        description = ''
+          Extra configuration for the widget
+        '';
+        apply = settings: if settings == null then {} else settings;
+      };
+    };
+
+    convert = 
+      { position
+      , size
+      , compactView
+      , settings
+      }: {
+        name = "org.kde.plasma.appmenu";
+        config = lib.recursiveUpdate {
+          General = lib.filterAttrs (_: v: v != null) {
+            inherit compactView;
+          };
+        } settings;
+    };
+  };
+}
