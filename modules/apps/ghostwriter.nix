b0c15313e158f4bf03317bd0d2c76d100c27dc7f
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-08-25 22:57:33 +0000

    Add themes

--- modules/apps/ghostwriter.nix
+++ modules/apps/ghostwriter.nix
@@ -1,6 +1,11 @@
 { config, lib, pkgs, ... }:
 let
   cfg = config.programs.ghostwriter;
+
+  createThemes = lib.attrsets.mapAttrs' (name: value: lib.attrsets.nameValuePair
+    ("ghostwriter/${name}.json")
+    ({ enable = true; source = value; })
+  );
 in
 {
   options.programs.ghostwriter = {
@@ -15,9 +20,30 @@ in
         `pkgs.kdePackages.ghostwriter` in Plasma6.
       '';
     };
+
+    theme = {
+      name = lib.mkOption {
+        type = with lib.types; nullOr str;
+        default = null;
+        example = "Ghostwriter";
+        description = ''
+          The name of the theme to use.
+        '';
+      };
+      customThemes = lib.mkOption {
+        type = with lib.types; attrsOf path;
+        default = { };
+        description = ''
+          Custom themes to be added to the installation. The key is their name.
+          Choose them in `programs.ghostwriter.theme.name`.
+        '';
+      };
+    };
   };
 
   config = (lib.mkIf cfg.enable {
     home.packages = [ cfg.package ];
+
+    xdg.dataFile = (createThemes cfg.theme.customThemes);
   });
 }
