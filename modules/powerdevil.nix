085a2fcf2a0296b180e7fa94337983d8d20ce588
Marcel Campello <marcel.campello@prafrentex.com.br>
2024-10-13 23:23:41 -0300

    Move assertions declarations to the 'let' clause

--- modules/powerdevil.nix
+++ modules/powerdevil.nix
@@ -458,6 +458,41 @@ let
       )
     ];
 
+    generateAssertionsForProfile = profile: [
+      {
+        assertion = (
+          cfg.powerdevil.${profile}.suspendSession.afterAPeriodOfInactivity.action != afterAPeriodOfInactivityActions.doNothing
+          || cfg.powerdevil.${profile}.suspendSession.afterAPeriodOfInactivity.idleTimeout == null
+        );
+        message = "Setting programs.plasma.powerdevil.${profile}.suspendSession.afterAPeriodOfInactivity.idleTimeout for autosuspend-action \"doNothing\" is not supported.";
+      }
+      {
+        assertion = (
+          cfg.powerdevil.${profile}.displayAndBrightness.turnOffScreen.idleTimeout != -1
+          || cfg.powerdevil.${profile}.displayAndBrightness.turnOffScreen.idleTimeoutWhenLocked == null
+        );
+        message = "Setting programs.plasma.powerdevil.${profile}.displayAndBrightness.turnOffScreen.idleTimeoutWhenLocked for idleTimeout \"never\" is not supported.";
+      }
+      {
+        assertion = (
+          cfg.powerdevil.${profile}.displayAndBrightness.changeScreenBrightness.enable != false
+          || cfg.powerdevil.${profile}.displayAndBrightness.changeScreenBrightness.percentage == null
+        );
+        message = "Cannot set programs.plasma.powerdevil.${profile}.displayAndBrightness.changeScreenBrightness.percentage when programs.plasma.powerdevil.${profile}.displayAndBrightness.changeScreenBrightness.enable is disabled.";
+      }
+    ];
+
+    generalAssertions = [
+      {
+        assertion = (
+          cfg.powerdevil.batteryLevels.lowLevel == null
+          || cfg.powerdevil.batteryLevels.criticalLevel == null
+          || cfg.powerdevil.batteryLevels.lowLevel > cfg.powerdevil.batteryLevels.criticalLevel
+        );
+        message = "programs.plasma.powerdevil.batteryLevels.criticalLevel cannot be greater than programs.plasma.powerdevil.batteryLevels.lowLevel.";
+      }
+    ];
+
 in
 {
   imports =
@@ -467,47 +502,10 @@ in
     ++ generalModifiedOptionsModules;
 
   config.assertions =
-    let
-      generateAssertionsForProfile = profile: [
-        {
-          assertion = (
-            cfg.powerdevil.${profile}.suspendSession.afterAPeriodOfInactivity.action != afterAPeriodOfInactivityActions.doNothing
-            || cfg.powerdevil.${profile}.suspendSession.afterAPeriodOfInactivity.idleTimeout == null
-          );
-          message = "Setting programs.plasma.powerdevil.${profile}.suspendSession.afterAPeriodOfInactivity.idleTimeout for autosuspend-action \"doNothing\" is not supported.";
-        }
-        {
-          assertion = (
-            cfg.powerdevil.${profile}.displayAndBrightness.turnOffScreen.idleTimeout != -1
-            || cfg.powerdevil.${profile}.displayAndBrightness.turnOffScreen.idleTimeoutWhenLocked == null
-          );
-          message = "Setting programs.plasma.powerdevil.${profile}.displayAndBrightness.turnOffScreen.idleTimeoutWhenLocked for idleTimeout \"never\" is not supported.";
-        }
-        {
-          assertion = (
-            cfg.powerdevil.${profile}.displayAndBrightness.changeScreenBrightness.enable != false
-            || cfg.powerdevil.${profile}.displayAndBrightness.changeScreenBrightness.percentage == null
-          );
-          message = "Cannot set programs.plasma.powerdevil.${profile}.displayAndBrightness.changeScreenBrightness.percentage when programs.plasma.powerdevil.${profile}.displayAndBrightness.changeScreenBrightness.enable is disabled.";
-        }
-      ];
-
-      generalAssertions = [
-        {
-          assertion = (
-            cfg.powerdevil.batteryLevels.lowLevel == null
-            || cfg.powerdevil.batteryLevels.criticalLevel == null
-            || cfg.powerdevil.batteryLevels.lowLevel > cfg.powerdevil.batteryLevels.criticalLevel
-          );
-          message = "programs.plasma.powerdevil.batteryLevels.criticalLevel cannot be greater than programs.plasma.powerdevil.batteryLevels.lowLevel.";
-        }
-      ];
-
-    in
-      (generateAssertionsForProfile "AC")
-      ++ (generateAssertionsForProfile "battery")
-      ++ (generateAssertionsForProfile "lowBattery")
-      ++ generalAssertions;
+    (generateAssertionsForProfile "AC")
+    ++ (generateAssertionsForProfile "battery")
+    ++ (generateAssertionsForProfile "lowBattery")
+    ++ generalAssertions;
 
   options = {
     programs.plasma.powerdevil = {
