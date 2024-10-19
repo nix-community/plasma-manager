b1b3098a09d42dead76889c9ef79bb0cd6c451c2
Fernando Rodrigues <alpha@sigmasquadron.net>
2024-10-19 19:50:42 +0000

    style(flake.nix): avoid repeating `home-manager` in inputs
    Signed-off-by: Fernando Rodrigues <alpha@sigmasquadron.net>

--- flake.nix
+++ flake.nix
@@ -4,8 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
