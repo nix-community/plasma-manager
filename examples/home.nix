a8953443d8cc1a274f405735161af164a0cf9c61
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-10-19 10:51:27 -0300

    chore(examples): add desktop widget example (#393)

--- examples/home.nix
+++ examples/home.nix
@@ -32,6 +32,21 @@
       };
     };
 
+    desktop.widgets = [
+      {
+        plasmusicToolbar = {
+          position = {
+            horizontal = 51;
+            vertical = 100;
+          };
+          size = {
+            width = 250;
+            height = 250;
+          };
+        };
+      }
+    ];
+
     panels = [
       # Windows-like panel at the bottom
       {
