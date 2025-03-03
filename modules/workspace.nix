356c9b9fd1bae2da2b3154688030ae2124786e1b
4lon <alon.nusem@gmail.com>
2025-03-03 18:54:14 +1000

    Added configuring of taskManagerFeedback and animationTime

--- modules/workspace.nix
+++ modules/workspace.nix
@@ -378,30 +378,39 @@ in
               }
             );
             klaunchrc = (
-              lib.mkIf (cfg.workspace.cursor != null && cfg.workspace.cursor.cursorFeedback != null) (
-	        lib.mkMerge [
-	          (lib.mkIf (cfg.workspace.cursor.cursorFeedback == "None") {
-                    BusyCursorSettings.Blinking = "false";
-                    BusyCursorSettings.Bouncing = "false";
-	            FeedbackStyle.BusyCursor = "false";
-	          })
-	          (lib.mkIf (cfg.workspace.cursor.cursorFeedback == "Static") {
-                    BusyCursorSettings.Blinking = "false";
-                    BusyCursorSettings.Bouncing = "false";
-	            FeedbackStyle.BusyCursor = "true";
-		  })
-	          (lib.mkIf (cfg.workspace.cursor.cursorFeedback == "Blinking") {
-                    BusyCursorSettings.Blinking = "true";
-                    BusyCursorSettings.Bouncing = "false";
-	            FeedbackStyle.BusyCursor = "true";
-		  })	
-	          (lib.mkIf (cfg.workspace.cursor.cursorFeedback == "Bouncing") {
-                    BusyCursorSettings.Blinking = "false";
-                    BusyCursorSettings.Bouncing = "true";
-	            FeedbackStyle.BusyCursor = "true";
-		  })	
-		]
-              )
+	      lib.mkMerge [
+                (lib.mkIf (cfg.workspace.cursor != null && cfg.workspace.cursor.cursorFeedback != null) (
+	          lib.mkMerge [
+	            (lib.mkIf (cfg.workspace.cursor.cursorFeedback == "None") {
+                      BusyCursorSettings.Blinking = "false";
+                      BusyCursorSettings.Bouncing = "false";
+	              FeedbackStyle.BusyCursor = "false";
+	            })
+	            (lib.mkIf (cfg.workspace.cursor.cursorFeedback == "Static") {
+                      BusyCursorSettings.Blinking = "false";
+                      BusyCursorSettings.Bouncing = "false";
+	              FeedbackStyle.BusyCursor = "true";
+		    })
+	            (lib.mkIf (cfg.workspace.cursor.cursorFeedback == "Blinking") {
+                      BusyCursorSettings.Blinking = "true";
+                      BusyCursorSettings.Bouncing = "false";
+	              FeedbackStyle.BusyCursor = "true";
+		    })	
+	            (lib.mkIf (cfg.workspace.cursor.cursorFeedback == "Bouncing") {
+                      BusyCursorSettings.Blinking = "false";
+                      BusyCursorSettings.Bouncing = "true";
+	              FeedbackStyle.BusyCursor = "true";
+		    })	
+		  ]
+                ))
+		(lib.mkIf (cfg.workspace.cursor != null && cfg.workspace.cursor.taskManagerFeedback != null) {
+		  FeedbackStyle.TaskbarButton = cfg.workspace.cursor.taskManagerFeedback;
+		})
+		(lib.mkIf (cfg.workspace.cursor != null && cfg.workspace.cursor.animationTime != null) {
+		  BusyCursorSettings.Timeout = cfg.workspace.cursor.animationTime; 
+		  TaskbarButtonSettings.Timeout = cfg.workspace.cursor.animationTime; 
+		})
+	      ]
             );
             ksplashrc.KSplash = (
               lib.mkIf (cfg.workspace.splashScreen.theme != null) {
