58aee3ba69d0dbce66f52ea53a82dc1dd5c1782b
Emma <contact@emma.pizza>
2025-04-05 12:23:05 +0200

    feat: add more assertions to basic test

--- test/basic.nix
+++ test/basic.nix
@@ -12,34 +12,43 @@ let
     export XDG_CONFIG_HOME=''${XDG_CONFIG_HOME:-$HOME/.config}
     export PATH=${kdePackages.kconfig}/bin:$PATH
 
-    kread_global() {
-      kreadconfig6 --file $XDG_CONFIG_HOME/kdeglobals "$@"
-    }
-
-    assert_eq() {
+    assert() {
+      file=$1
+      shift
       want=$1
       shift
-      actual=$(kread_global "$@")
+      actual=$(kreadconfig6 --file $file "$@")
 
       if [ "$actual" != "$want" ]; then
         echo >&2 "ERROR: $@: expected $want but got $actual"
         exit 1
+      else
+        echo >&2 "OK: got $want"
       fi
     }
 
-    assert_eq false --group KDE --key SingleClick
+    assert kdeglobals false --group KDE --key SingleClick
     # Set with shorthand
-    assert_eq 1 --group group --key key1
+    assert kdeglobals 1 --group group --key key1
     # Set with longhand and immutable
-    assert_eq 2 --group group --key key2
+    assert kdeglobals 2 --group group --key key2
     # Nested groups, with group containing /
-    assert_eq 3 --group escaped/nested --group group --key key3
+    assert kdeglobals 3 --group escaped/nested --group group --key key3
     # Value and key have leading space
-    assert_eq " leading space" --group group --key " leading space"
+    assert kdeglobals " leading space" --group group --key " leading space"
     # Set outside plasma-manager, value has leading space, group contains /
-    assert_eq " value" --group escaped/nested --group group --key untouched
+    assert kdeglobals " value" --group escaped/nested --group group --key untouched
     # Escaped key with shell expansion
-    assert_eq "/home/fake" --group group --key 'escaped[$i]'
+    assert kdeglobals "/home/fake" --group group --key 'escaped[$i]'
+
+    assert kwinrc testvalue --group testgroup --key testkey
+    assert kwinrc true --group Plugins --key somePluginEnabled
+    assert kwinrc MMM --group "org.kde.kdecoration2" --key ButtonsOnLeft
+    assert kwinrc A --group "org.kde.kdecoration2" --key ButtonsOnRight
+    assert kwinrc "/run/current-system/sw/share/applications/com.github.maliit.keyboard.desktop" --group Wayland --key InputMethod
+    
+    assert kglobalshortcutsrc 'Meta+F' --group "services" --group "firefox.desktop" --key "new-window"
+    assert kglobalshortcutsrc 'bar' --group "services" --group "firefox.desktop" --key "foo"
   '';
 in
 testers.nixosTest {
@@ -54,15 +63,28 @@ testers.nixosTest {
       isNormalUser = true;
     };
 
+    home-manager.sharedModules = [
+      plasma-module
+      {
+        programs.plasma.shortcuts."services/firefox.desktop"."new-window" = [ "Meta+F" ];
+      }
+    ];
+
     home-manager.users.fake =
       { lib, ... }:
       {
         home.stateVersion = "23.11";
-        imports = [ plasma-module ];
         programs.plasma = {
           enable = true;
           workspace.clickItemTo = "select";
-          # Test a variety of weird keys and groups
+          kwin.titlebarButtons.right = [ "maximize" ];
+          configFile."kwinrc" = {
+            "testgroup"."testkey" = "testvalue";
+            "Plugins"."somePluginEnabled" = true;
+            "org.kde.kdecoration2".ButtonsOnLeft = "MMM";
+            "Wayland"."InputMethod" = "/run/current-system/sw/share/applications/com.github.maliit.keyboard.desktop";
+          };
+          configFile."kglobalshortcutsrc"."services/firefox.desktop"."foo" = "bar";
           configFile.kdeglobals = {
             group = {
               " leading space" = " leading space";
@@ -100,10 +122,12 @@ testers.nixosTest {
     machine.wait_until_succeeds(
         "systemctl show -p ActiveState --value home-manager-fake.service | grep -q 'inactive' && " +
         "systemctl show -p Result --value home-manager-fake.service | grep -q 'success'"
-    )
+    , 10)
 
     # Run tests:
     machine.succeed("test -e /home/fake/.config/kdeglobals")
+    machine.succeed("test -e /home/fake/.config/kwinrc")
+    machine.succeed("test -e /home/fake/.config/kglobalshortcutsrc")
     machine.succeed("su - fake -c plasma-basic-test")
   '';
 }
