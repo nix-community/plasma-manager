nix-community/plasma-manager

commit 88e41436997e9badacafa057969a2bea3a55e48c

Allow setting different wallpapers for different desktops (#470)

modules/workspace.nix
@@ -161,7 +161,7 @@ in
     };
 
     wallpaper = lib.mkOption {
-      type = with lib.types; nullOr path;
+      type = with lib.types; nullOr (either path (listOf path));
       default = null;
       example = lib.literalExpression ''"''${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Kay/contents/images/1080x1920.png"'';
       description = ''
@@ -453,7 +453,23 @@ in
               for (const desktop of allDesktops) {
                   desktop.wallpaperPlugin = "org.kde.image";
                   desktop.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"];
-                  desktop.writeConfig("Image", "file://${toString cfg.workspace.wallpaper}");
+                  ${
+                    if (builtins.typeOf cfg.workspace.wallpaper) == "list" then
+                      ''
+                        let images = [
+                        ${
+                          builtins.concatStringsSep "\n," (map (
+                            wallpaper: ''"file://${toString wallpaper}"''
+                          ) cfg.workspace.wallpaper)
+                        }
+                        ];
+                        let image = images[desktop.screen];''
+                    else
+                      ''let image = "file://${toString cfg.workspace.wallpaper}""''
+                  }
+                  if (image) {
+                    desktop.writeConfig("Image", image);
+                  }
                   ${
                     lib.optionalString (cfg.workspace.wallpaperFillMode != null)
                       ''desktop.writeConfig("FillMode", "${

commit 84ef10e7972e7007259b98a64f4a0ef545fbc20f

fix: fixed string-based wallpaper regression introduced in commit 88e41436997e9badacafa057969a2bea3a55e48c

modules/workspace.nix
@@ -465,7 +465,7 @@ in
                         ];
                         let image = images[desktop.screen];''
                     else
-                      ''let image = "file://${toString cfg.workspace.wallpaper}""''
+                      ''let image = "file://${toString cfg.workspace.wallpaper}";''
                   }
                   if (image) {
                     desktop.writeConfig("Image", image);