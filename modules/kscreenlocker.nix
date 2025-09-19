e1cca9fc44534424dfc476e68449e1c0561993c3
Antonio Subašić <antonio.subasic.public@gmail.com>
2025-09-19 18:22:05 +0200

    fix: update kscreenlocker wallpaper configuration
    previous implementation dropped file into nix store, leaving it
    unreferenced, causing garbage collection to delete it
    
    builtins.path { path = ...; } ensures the file is not garbage collected

--- modules/kscreenlocker.nix
+++ modules/kscreenlocker.nix
@@ -202,9 +202,9 @@ in
       lib.mkMerge [
         (lib.mkIf (cfg.kscreenlocker.appearance.wallpaper != null) {
           Greeter.WallpaperPlugin = "org.kde.image";
-          "Greeter/Wallpaper/org.kde.image/General".Image = (
-            builtins.toString cfg.kscreenlocker.appearance.wallpaper
-          );
+          "Greeter/Wallpaper/org.kde.image/General".Image = builtins.path {
+            path = cfg.kscreenlocker.appearance.wallpaper;
+          };
         })
         (lib.mkIf (cfg.kscreenlocker.appearance.wallpaperPictureOfTheDay != null) {
           Greeter.WallpaperPlugin = "org.kde.potd";
