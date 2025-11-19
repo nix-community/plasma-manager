e0ff5751ac1c3a1a05faf27065f0694a1abbc181
paigely <235818692+paigely@users.noreply.github.com>
2025-11-18 20:43:04 -0600

    feat(workspace): add widgetStyle option

--- modules/workspace.nix
+++ modules/workspace.nix
@@ -273,6 +273,15 @@ in
         '';
       };
     };
+
+    widgetStyle = lib.mkOption {
+      type = with lib.types; nullOr str;
+      default = null;
+      example = "breeze";
+      description = ''
+        The widget style to use with Plasma.
+      '';
+    };
   };
 
   config = (
@@ -343,6 +352,9 @@ in
             kdeglobals = {
               KDE.SingleClick = (
                 lib.mkIf (cfg.workspace.clickItemTo != null) (cfg.workspace.clickItemTo == "open")
+              );
+              KDE.widgetStyle = (
+                lib.mkIf (cfg.workspace.widgetStyle != null) (cfg.workspace.widgetStyle)
               );
               Sounds.Theme = (lib.mkIf (cfg.workspace.soundTheme != null) cfg.workspace.soundTheme);
             };
