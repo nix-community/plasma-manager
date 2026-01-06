21491638093876d7409f497aed5fecbb3a6e61c7
Misaka13514 <Misaka13514@gmail.com>
2026-01-06 08:54:24 +0800

    fix(input): replace splitString with stringToCharacters, fix eval on Darwin

--- modules/input.nix
+++ modules/input.nix
@@ -367,25 +367,8 @@ in
   config.assertions = [
     (
       let
-        validChars = [
-          "0"
-          "1"
-          "2"
-          "3"
-          "4"
-          "5"
-          "6"
-          "7"
-          "8"
-          "9"
-          "a"
-          "b"
-          "c"
-          "d"
-          "e"
-          "f"
-        ];
-        hexChars = hexStr: builtins.tail (lib.reverseList (builtins.tail (lib.splitString "" hexStr)));
+        validChars = lib.stringToCharacters "0123456789abcdef";
+        hexChars = lib.stringToCharacters;
         hexCodeInvalid =
           hex:
           !(lib.all (c: builtins.elem (lib.toLower c) validChars) (hexChars hex))
